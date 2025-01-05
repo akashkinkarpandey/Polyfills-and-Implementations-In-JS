c68fd34376bb98d3a91723734b88b7c277fb7e78
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-01-05 22:02:13 +0530

    added documentation to Observer pattern class using github copilot

--- implementations/Observer-Pattern.js
+++ implementations/Observer-Pattern.js
@@ -1,34 +1,73 @@
-class Host{
-    constructor(){
-        this.observers=[]
+/**
+ * Class representing a Host that manages observers.
+ */
+class Host {
+    constructor() {
+        /**
+         * Array to store registered observers.
+         * @type {Function[]}
+         */
+        this.observers = [];
     }
-    registerObserver(observer){
-        this.observers.push(observer)
+
+    /**
+     * Registers an observer.
+     * @param {Function} observer - The observer to be registered.
+     */
+    registerObserver(observer) {
+        this.observers.push(observer);
     }
-    unregisterObserver(observer){
-        this.observers=this.observers.filter(eachObserver=>eachObserver!==observer)
+
+    /**
+     * Unregisters an observer.
+     * @param {Function} observer - The observer to be unregistered.
+     */
+    unregisterObserver(observer) {
+        this.observers = this.observers.filter(eachObserver => eachObserver !== observer);
     }
-    notifyObserver(callOberserverWithArgument,thisContext){
-        let actualContext=thisContext||this
+
+    /**
+     * Notifies all registered observers.
+     * @param {*} callOberserverWithArgument - The argument to be passed to the observers.
+     * @param {Object} [thisContext] - The context in which the observer functions should be called.
+     */
+    notifyObserver(callOberserverWithArgument, thisContext) {
+        let actualContext = thisContext || this;
         this.observers.forEach((eachObserver) =>
           eachObserver.call(actualContext, callOberserverWithArgument)
         );
     }
 }
-const observer1=function(data){
+
+/**
+ * Observer function that logs a message with the provided data.
+ * @param {*} data - The data to be logged.
+ */
+const observer1 = function(data) {
     console.log(`I am observer1 with event-> ${data}`);
-}
-const observer2 = function (data) {
-  console.log(`I am observer2 with event-> ${data}`);
 };
-const observer3 = function (data) {
-  console.log(`I am observer3 with event-> ${data}`);
+
+/**
+ * Observer function that logs a message with the provided data.
+ * @param {*} data - The data to be logged.
+ */
+const observer2 = function(data) {
+    console.log(`I am observer2 with event-> ${data}`);
 };
-const host=new Host()
-host.registerObserver(observer2)
-host.notifyObserver('notifying first time')
+
+/**
+ * Observer function that logs a message with the provided data.
+ * @param {*} data - The data to be logged.
+ */
+const observer3 = function(data) {
+    console.log(`I am observer3 with event-> ${data}`);
+};
+
+const host = new Host();
+host.registerObserver(observer2);
+host.notifyObserver('notifying first time');
 host.registerObserver(observer3);
 host.registerObserver(observer1);
-host.notifyObserver(`notifying 2nd time`)
-host.unregisterObserver(observer2)
+host.notifyObserver(`notifying 2nd time`);
+host.unregisterObserver(observer2);
 host.notifyObserver(`notifying 3rd time`);
