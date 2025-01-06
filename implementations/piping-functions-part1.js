edb70d0a33dfac15289fbe38d056c0e42ad6723f
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-01-06 20:13:25 +0530

    piping functions part 1 added

---
+++ implementations/piping-functions-part1.js
@@ -0,0 +1,28 @@
+// Given an object which can have a function as a value at a nested level,
+// We need to create a function that will accept arguments as input and pass it through all the functions in the input object and return the computed value.
+function first(obj) {
+  return function(...argumentsToFunctionInObj) {
+    for (const key in obj) {
+        const value = obj[key];
+        if(typeof value==='function'){
+            obj[key] = value(...argumentsToFunctionInObj);
+        }
+        else{
+            obj[key]=first(value)(...argumentsToFunctionInObj);
+        }
+    }
+    return obj;
+  };
+}
+const obj = {
+  a: {
+    b: (a, b, c) => a + b + c,
+    c: (a, b, c) => a + b + c,
+  },
+  d: (a, b, c) => a + b + c,
+};
+console.log(first(obj)(2, 3, 4));//gives correct output
+//{a: {b: 9, c: 9}
+//d : 9 }
+console.log(first(obj)(1, 1, 1));//gives same output as previous as original object is mutated,needs to be fixed
+
