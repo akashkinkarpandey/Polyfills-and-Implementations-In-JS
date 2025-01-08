87cf7015bba39b9ec5f26cc2ec0cd0c9a71c978f
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-01-08 20:17:15 +0530

    Method chaining-good coding practice included

--- implementations/method-chaining-part-1.js
+++ implementations/method-chaining-part-1.js
@@ -22,8 +22,8 @@ const calculatorObj={
 const finalObject=calculatorObj.add(10).subtract(11).multiply(-5).divide(2)
 console.log(finalObject.result);
 //way 2 using constructor functions
-const calculatorFunction=function(){
-  this.result=0;//let result won't work as it is not bind with this keyword 
+const CalculatorFunction=function(){
+  this.result=0;//let result won't work as it is not cound with this keyword 
   //const add=()=>{} won't work neither will function add(){} work.
   this.add=function(val){
     this.result += val;
@@ -45,7 +45,7 @@ const calculatorFunction=function(){
     return this.result;
   };
 };
-const calculator2 = new calculatorFunction();
+const calculator2 = new CalculatorFunction();
 const result=calculator2.add(2).subtract(4).multiply(-3).divide(6).getResult();
 console.log(result);
 
