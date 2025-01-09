20d377b9f388f498302b85ad769f8b0d42a2a10d
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-01-09 20:31:35 +0530

    filtering multi dimensional array implementation

---
+++ implementations/filter-multidimensional-array.js
@@ -0,0 +1,20 @@
+const filterMultiDimensionalArray=function(array,filterCondition){
+    const filteredElements=[]
+    for(const element of array){
+        if(Array.isArray(element)){
+            const resultFromNestedArray = filterMultiDimensionalArray(
+              element,
+              filterCondition
+            );
+            filteredElements.push(resultFromNestedArray)
+        }else{
+            if(filterCondition(element))
+                filteredElements.push(element)      
+        }
+    }
+    return filteredElements;
+}
+const array=[['a',['b',[true,['d',['e',['f',{a:'a'}],9]]]]]]
+const filterCondition=(e)=>typeof e==="string";
+console.log(filterMultiDimensionalArray(array,filterCondition).toString());
+//a,b,d,e,f
