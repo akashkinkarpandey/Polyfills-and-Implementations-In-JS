5d936abfdad409a39ecf7021aba3759f2ce8cdc5
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-01-09 20:33:56 +0530

    filtering multi dimensional array in better output format

--- implementations/filter-multidimensional-array.js
+++ implementations/filter-multidimensional-array.js
@@ -18,3 +18,5 @@ const array=[['a',['b',[true,['d',['e',['f',{a:'a'}],9]]]]]]
 const filterCondition=(e)=>typeof e==="string";
 console.log(filterMultiDimensionalArray(array,filterCondition).toString());
 //a,b,d,e,f
+console.log(JSON.stringify(filterMultiDimensionalArray(array, filterCondition)));
+// [["a", ["b", [["d", ["e", ["f"]]]]]]];
