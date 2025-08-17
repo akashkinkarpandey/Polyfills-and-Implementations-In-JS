754432fc93bad1de0cf42106191f9cce15966c72
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-08-17 13:56:42 +0530

    Corrected map, filter implementations

--- implementations/custom-filter-function.js
+++ implementations/custom-filter-function.js
@@ -8,14 +8,19 @@ console.log(newArr);
 const arr1 = [2, 5, 6, 10,13,14,11];
 Array.prototype.myFilter = function (callbackFunction) {
   const newArr = [];
-  for (const element of this) {
-    if(callbackFunction(element, newArr.length + 1, this))
-        newArr.push(element)
+  // for (const element of this) {
+  //   if(callbackFunction(element, newArr.length + 1, this))
+  //       newArr.push(element)
+  // }
+  for (let i = 0; i < this.length; i++) {
+    if (callbackFunction(this[i], i, this)) {
+      newArr.push(this[i]);
+    }
   }
   return newArr;
 };
-const newArr2 = arr1.filter((x, i, arr) => {
+const newArr2 = arr1.myFilter((x, i, arr) => {
   console.log(x, i, arr);
-  return x % 2==1;
+  return x % 2 == 1;
 });
 console.log(newArr2);
--- implementations/custom-map-function.js
+++ implementations/custom-map-function.js
@@ -8,13 +8,13 @@ console.log(newArr);
 const arr1=[2,5,6,10]
 Array.prototype.myMap=function(callbackFunction){
     const newArr=[]
-    for(const element of this){
-        newArr.push(callbackFunction(element,newArr.length+1,this))
+    for (let i = 0; i < this.length; i++) {
+      newArr.push(callbackFunction(this[i], i, this));
     }
     return newArr
 }
-const newArr2=arr1.map((x,i,arr)=>{
-    console.log(x,i,arr);
-    return x*2
-})
+const newArr2 = arr1.myMap((x, i, arr) => {
+  console.log(x, i, arr);
+  return x * 2;
+});
 console.log(newArr2);
